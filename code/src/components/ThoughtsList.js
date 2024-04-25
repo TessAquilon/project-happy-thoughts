755106a5f04803df3e01c600ff645f41996efee4
Nino Aquilon <nino.aquilon@gmail.com>
2024-04-25 10:18:20 +0200

    svg spinner

--- code/src/components/ThoughtsList.js
+++ code/src/components/ThoughtsList.js
@@ -11,7 +11,18 @@ const ThoughtsList = ({ loading, thoughtsList, handleLike }) => {
   if (loading) {
     return (
       <div className="loading-div">
-        <h1>Loading in progress...</h1>
+        <svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg" width="70" height="70">
+          <circle
+            className="spin"
+            cx="400"
+            cy="400"
+            fill="none"
+            r="200"
+            strokeWidth="50"
+            stroke="var(--black)"
+            strokeDasharray="700 1400"
+            strokeLinecap="round" />
+        </svg>
       </div>
     )
   }
