1635ddd04c63602cd7d3655181a8176310d959cc
Nino Aquilon <nino.aquilon@gmail.com>
2024-04-25 10:08:39 +0200

    links

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -11,10 +11,10 @@ export const Footer = () => {
       <hr className="divider" />
       <p>A website created by Nino Aquilon @Technigo &apos;23.</p>
       <section className="contact">
-        <a href="https://www.github.com" className="button icon-button no-glow" aria-label="Go to my Github">
+        <a href="https://github.com/codeandjazz" className="button icon-button no-glow" aria-label="Go to my Github">
           <FontAwesomeIcon icon={faGithub} />
         </a>
-        <a href="https://www.github.com" className="button icon-button no-glow" aria-label="Go to my LinkedIn">
+        <a href="https://www.linkedin.com/in/ninoaquilon/" className="button icon-button no-glow" aria-label="Go to my LinkedIn">
           <FontAwesomeIcon icon={faLinkedin} />
         </a>
       </section>
